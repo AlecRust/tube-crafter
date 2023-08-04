efb1d4f7746d13c56e0302622d2050c1adf3fe78
Alec Rust <me@alecrust.com>
2023-08-04 22:18:13 +0100

    Rename "script" to "input"

--- src/index.js
+++ src/index.js
@@ -10,53 +10,66 @@ const imageGenerator = require('./imageGenerator');
 const videoComposer = require('./videoComposer');
 
 const argv = yargs(hideBin(process.argv))
-    .command('$0 <script>', 'Process the script', (yargs) => {
-        yargs.positional('script', {
-            describe: 'Path to the script file',
+    .command('$0 <inputFile>', 'Process the input file', (yargs) => {
+        yargs.positional('inputFile', {
+            describe: 'Path to the input file',
             type: 'string',
         });
     })
-    .demandCommand(1, 'You need to provide a script file')
+    .demandCommand(1, 'You need to provide an input file')
     .help()
     .argv;
 
-const scriptPath = path.resolve(argv.script);
+const inputFile = path.resolve(argv.inputFile);
 
-async function processParagraphs(paragraphs, outputDir) {
-  const processingPromises = paragraphs.map((paragraph, i) => {
+async function processParagraph(paragraph, outputDir, i) {
+  try {
       const audioPath = path.join(outputDir, 'audio', `${i}.mp3`);
       const imagePath = path.join(outputDir, 'images', `${i}.jpg`);
-      return Promise.all([
+
+      await Promise.all([
           textToSpeech(paragraph, audioPath),
           imageGenerator(paragraph, imagePath),
       ]);
-  });
-  return Promise.all(processingPromises);
+  } catch (error) {
+      console.error(`An error occurred while processing paragraph ${i}: ${error.message}`);
+      process.exit(1);
+  }
+}
+
+async function processParagraphs(paragraphs, outputDir) {
+  const processingPromises = paragraphs.map((paragraph, i) => processParagraph(paragraph, outputDir, i));
+  await Promise.all(processingPromises);
+}
+
+function getParagraphsFromTokens(tokens) {
+    return tokens.reduce((paragraphs, token, i) => {
+        if (token.type === 'paragraph_open') {
+            paragraphs.push(tokens[i + 1].content);
+        }
+        return paragraphs;
+    }, []);
 }
 
 async function main() {
-    if (!fs.existsSync(scriptPath)) {
-        console.error(`The script file ${scriptPath} does not exist`);
+    if (!fs.existsSync(inputFile)) {
+        console.error(`The input file ${inputFile} does not exist`);
         process.exit(1);
     }
 
     try {
-        const script = await fs.readFile(scriptPath, 'utf8');
+        const input = await fs.readFile(inputFile, 'utf8');
 
         const md = new MarkdownIt();
-        const tokens = md.parse(script, {});
-
-        // console.log('Tokens:', tokens);
-
-        const paragraphs = [];
-        for (let i = 0; i < tokens.length; i++) {
-            const token = tokens[i];
-
-            if (token.type === 'paragraph_open') {
-                const content = tokens[i + 1].content;
-                paragraphs.push(content);
-            }
+        let tokens;
+        try {
+            tokens = md.parse(input, {});
+        } catch (error) {
+            console.error(`Failed to parse input file as Markdown: ${error.message}`);
+            process.exit(1);
         }
+
+        const paragraphs = getParagraphsFromTokens(tokens);
 
         console.log(`🏁 Found ${paragraphs.length} paragraphs to process`);
 
@@ -73,7 +86,7 @@ async function main() {
           outputDir,
         );
     } catch (error) {
-        console.error(`An error occurred while processing the script: ${error.message}`);
+        console.error(`An error occurred while processing the input file: ${error.message}`);
         process.exit(1);
     }
 }
